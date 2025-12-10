3e79f849cc5aede423c713bd92a2375bcd55fa19
AmirHossein Biabani <amirhosseinbyabani@yahoo.com>
2025-12-10 15:48:41 +0330

    User and Request at Domain Crated .

---
+++ Domain/Requests/request.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Users;
+
+namespace Domain.request
+{
+    public class Request
+    {
+        public long RegustId { get; set; }
+        public required User RegustedUser { get; set; }
+        public string? Reason { get; set; }
+        public DateTime CratedDate {  get; set; }
+        public DateTime StardDate {  get; set; }
+        public DateTime FinishDateDate {  get; set; }
+    }
+}
